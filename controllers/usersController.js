2f597ca4659199a6193ee1738597f796073472d1
IbrahimHam <42837548+IbrahimHam@users.noreply.github.com>
2024-07-13 17:28:48 +0200

    Added getOrdersByUserId and updateUserToInactive endpoints

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -8,8 +8,8 @@ const getUsers = async (req, res) => {
         }
         return res.json(result.rows)
 
-    } catch (err) {
-        return res.status(500).send(err.message);
+    } catch (error) {
+        return res.status(500).send(error.message);
     }
 }
 
@@ -22,7 +22,7 @@ const getUserById = async (req, res) => {
         }
         return res.send(result.rows)
     } catch (error) {
-        return res.status(500).send(err.message);
+        return res.status(500).send(error.message);
     }
 }
 
@@ -32,7 +32,7 @@ const createUser = async (req, res) => {
         result = await db.query("INSERT INTO users (first_name, last_name, age) VALUES ($1 , $2, $3) RETURNING *", [first_name, last_name, age])
         res.send(result.rows)
     } catch (error) {
-        return res.status(500).send(err.message);
+        return res.status(500).send(error.message);
     }
 }
 
@@ -46,7 +46,7 @@ const updateUserById = async (req, res) => {
         }
         res.send(result.rows)
     } catch (error) {
-        return res.status(500).send(err.message);
+        return res.status(500).send(error.message);
     }
 }
 
@@ -59,8 +59,57 @@ const deleteUser = async (req, res) => {
         }
         res.send({ success: 'User was deleted' })
     } catch (error) {
-        return res.status(500).send(err.message);
+        return res.status(500).send(error.message);
     }
 }
 
-module.exports = { getUsers, getUserById, createUser, updateUserById, deleteUser }
+const getOrdersByUserId = async (req, res) => {
+    const userId = req.params.id;
+    try {
+        const result = await db.query(`
+            SELECT u.id as user_id, u.first_name, u.last_name, o.id AS order_id, o.price 
+            FROM users AS u
+            LEFT JOIN orders AS o
+            ON u.id = o.user_id
+            WHERE u.id = $1`, [userId])
+        if (result.rows.length === 0) {
+            return res.status(404).send('User not found');
+        }
+        res.send(result.rows)
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const updateUserToInactive = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const orderCheckResult = await db.query(
+            'SELECT COUNT(*) AS order_count FROM orders WHERE user_id = $1',
+            [id]
+        );
+
+        const orderCount = parseInt(orderCheckResult.rows[0].order_count, 10);
+
+        if (orderCount !== 0) {
+            return res.send('User has existing orders, not setting inactive');
+        }
+
+        const updateResult = await db.query(
+            'UPDATE users SET active = FALSE WHERE id = $1 RETURNING *',
+            [id]
+        );
+
+        if (updateResult.rows.length === 0) {
+            return res.status(404).send('User not found');
+        }
+
+        return res.json(updateResult.rows);
+
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+module.exports = { getUsers, getUserById, createUser, updateUserById, deleteUser, getOrdersByUserId, updateUserToInactive }
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers, getUserById, createUser, updateUserById, deleteUser} = require('../controllers/usersController');
+const { getUsers, getUserById, createUser, updateUserById, deleteUser, getOrdersByUserId,updateUserToInactive } = require('../controllers/usersController');
 
 router.get('/', getUsers)
 
@@ -11,5 +11,9 @@ router.post('/', createUser)
 router.put('/:id', updateUserById)
 
 router.delete('/:id', deleteUser)
+
+router.get('/:id/orders', getOrdersByUserId)
+
+router.put('/:id/check_inactive', updateUserToInactive)
 
 module.exports = router;
