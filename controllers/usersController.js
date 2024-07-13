ba2c62ed38b75e1b3666824b06ca6253b310b0d1
IbrahimHam <42837548+IbrahimHam@users.noreply.github.com>
2024-07-13 12:33:41 +0200

    Added validation on updateUser, deleteUser, getUserById

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -17,6 +17,9 @@ const getUserById = async (req, res) => {
     try {
         const id = req.params.id;
         const result = await db.query("SELECT * FROM users WHERE id = $1", [id])
+        if (result.rows.length === 0) {
+            return res.status(404).send('User not found');
+        }
         res.send(result.rows)
     } catch (error) {
         return res.status(500).send(err.message);
@@ -38,6 +41,9 @@ const updateUserById = async (req, res) => {
         const id = req.params.id;
         const { first_name, last_name, age } = req.body;
         const result = await db.query("UPDATE users SET first_name = $1, last_name = $2, age = $3 WHERE id = $4 RETURNING *", [first_name, last_name, age, id]);
+        if (result.rows.length === 0) {
+            return res.status(404).send('User not found');
+        }
         res.send(result.rows)
     } catch (error) {
         return res.status(500).send(err.message);
@@ -47,7 +53,10 @@ const updateUserById = async (req, res) => {
 const deleteUser = async (req, res) => {
     try {
         const id = req.params.id;
-        const result = await db.query("DELETE FROM users WHERE id = $1 RETURNING *", [id])
+        const result = await db.query("DELETE FROM users WHERE id = $1 RETURNING *", [id]);
+        if (result.rows.length === 0) {
+            return res.status(404).send('User not found');
+        }
         res.send(result.rows)
     } catch (error) {
         return res.status(500).send(err.message);
